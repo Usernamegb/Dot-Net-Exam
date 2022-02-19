f9d072dc93a8f08230f431853edd02146803eb5b
Gaurav Balaji Jadhav <88846632+Usernamegb@users.noreply.github.com>
2022-02-19 18:20:13 +0530

    Add files via upload

---
+++ WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _210940320041_WebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
